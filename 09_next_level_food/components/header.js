ced874eef844759b7cc9a36a61418eb7eb450ea0
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-07-10 13:29:21 +0530

    added custom component

--- 09_next_level_food/components/header.js
+++ 09_next_level_food/components/header.js
@@ -1,10 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import classes from "./header.module.css";
+import HeaderBackground from "./headerBackground";
+
 const Header = () => {
   return (
     <>
-      <img src="/logo.png" alt="" />
-      <h1>Welcome to this NextJS Course</h1>
+      <HeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logo} alt="A Plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
-}; 
+};
 
 export default Header;
